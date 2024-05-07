2591408b5a8008327addf4253f6f82a4de2145c2
Benedikt Heck <bheck@student.ethz.ch>
2024-05-07 12:53:40 +0200

    fix_2

--- project_3/template_solution.py
+++ project_3/template_solution.py
@@ -297,12 +297,12 @@ def train_model(train_loader):
     
         scheduler.step()
 
-        correct_ratio = float(correct_predictions)/len(validation_set.dataset)
-        print(f"epoch: {epoch} train set correct predictions: {correct_predictions} / {len(validation_set.dataset)} = {correct_ratio}")
-
         avg_loss = float(loss_sum)/len(training_set)
         training_loss.append(avg_loss)
         print(f'epoch: {epoch} training_loss: {avg_loss}')
+
+        correct_ratio = float(correct_predictions)/len(validation_set.dataset)
+        print(f"epoch: {epoch} train set correct predictions: {correct_predictions} / {len(validation_set.dataset)} = {correct_ratio}")
 
         correct_predictions = 0
 
